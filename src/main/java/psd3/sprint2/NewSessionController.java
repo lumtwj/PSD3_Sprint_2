8d3ca82cbb8e4148a5289fa3c06ab9474d21088c
Joe Lum <lumtwj@gmail.com>
2013-12-01 16:26:26 +0800

    Update of HTML files

--- src/main/java/psd3/sprint2/NewSessionController.java
+++ src/main/java/psd3/sprint2/NewSessionController.java
@@ -1,5 +1,8 @@
 package psd3.sprint2;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,7 +14,7 @@ import psd3.sprint2.object.SessionSetup;
 
 @Controller
 public class NewSessionController {
-	@RequestMapping(value="/NewSession", method=RequestMethod.GET)
+	@RequestMapping(value="NewSession", method=RequestMethod.GET)
 	public String newSession(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
 		//Load form
 		model.addAttribute("name", name);
@@ -20,7 +23,7 @@ public class NewSessionController {
 		return "NewSession";
 	}
 
-	@RequestMapping(value="/NewSession", method=RequestMethod.POST)
+	@RequestMapping(value="NewSession", method=RequestMethod.POST)
 	public String saveSession(@ModelAttribute(value="sess") SessionSetup s, Model model) {
 		//Process form
 		model.addAttribute("name", "Successfully saved session:" +
@@ -32,13 +35,18 @@ public class NewSessionController {
 				" Compulsory: " + s.isCompulsary() + 
 				" Venue: " + s.getVenue()
 				);
+
 		//Save to database
-		
+		try {
+			FileWriter f = new FileWriter("newSession.csv", true);
+			f.write(s.getDate() + "," + s.getDuration() + "," + s.getRepeatFrequency() + "," + s.getLecturer() + "," + s.getMaxAttendance() + "," + s.isCompulsary() + "," + s.getVenue() + "\n");
+			f.close();
+		} 
+		catch (IOException e) {e.printStackTrace();}
+
 		//Clear the forms
 		model.addAttribute("sess", new SessionSetup());
 
 		return "NewSession";
 	}
-
-
 }
