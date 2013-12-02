357cb9d964f4b9274336cafcdf7a1fc9ee1e6bee
Joe Lum <lumtwj@gmail.com>
2013-12-02 12:14:06 +0800

    Correct all files.

--- src/main/java/psd3/sprint2/LoginController.java
+++ src/main/java/psd3/sprint2/LoginController.java
@@ -12,9 +12,9 @@ import psd3.sprint2.object.SessionSetupLogin;
 public class LoginController {
 	@RequestMapping(value="login", method=RequestMethod.GET)
 	public String login(Model model) {
-		//Load form
 		model.addAttribute("sesslogin", new SessionSetupLogin());
 		model.addAttribute("name","");
+
 		return "login";
 	}
 
@@ -22,8 +22,8 @@ public class LoginController {
 	@RequestMapping(value="login", method=RequestMethod.POST)
 	public String saveSession(@ModelAttribute(value="sesslogin") SessionSetupLogin sesslogin, Model model) {
 		//Process form
-		
-		
+
+
 		if (sesslogin.getLogin().equals("admin") && sesslogin.getPassword().equals("admin")) {
 			return "redirect:/NewSession";
 		}
@@ -32,27 +32,6 @@ public class LoginController {
 		}else{
 			model.addAttribute("name","Wrong password/username");
 		}
-		
-		/*model.addAttribute("name", "Successfully saved session:" +
-				" Date: " + s.getDate() + 
-				" Duration: " + s.getDuration() +
-				" Repeat frequency: " + s.getRepeatFrequency() + 
-				" Lecturer: " + s.getLecturer() +
-				" Max attendance: " + s.getMaxAttendance() + 
-				" Compulsory: " + s.isCompulsary() + 
-				" Venue: " + s.getVenue()
-				);
-
-		//Save to database
-		try {
-			FileWriter f = new FileWriter("newSession.csv", true);
-			f.write(s.getDate() + "," + s.getDuration() + "," + s.getRepeatFrequency() + "," + s.getLecturer() + "," + s.getMaxAttendance() + "," + s.isCompulsary() + "," + s.getVenue() + "\n");
-			f.close();
-		} 
-		catch (IOException e) {e.printStackTrace();}
-
-		//Clear the forms
-		model.addAttribute("sess", new SessionSetup());*/
 
 		return "login";
 	}
